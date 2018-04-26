b169986de487a4d0d01cb6b7011767aedb2670e2
Ken Yu <cn.ken.yu@gmail.com>
2018-04-26 19:11:35 -0400

    Add settings for n2r.

---
+++ scripts/test_n2r.sh
@@ -0,0 +1,2 @@
+python test.py --dataroot ./datasets/rigid --name n2r_pix2pix --model pix2pix --which_model_netG unet_256\
+    --which_direction BtoA --dataset_mode randomly_masked --norm batch
---
+++ scripts/train_n2r.sh
@@ -0,0 +1,2 @@
+python train.py --dataroot ./datasets/rigid --name n2r_pix2pix --model pix2pix --which_model_netG unet_256\
+    --which_direction BtoA --lambda_A 100 --dataset_mode randomly_masked --no_lsgan --norm batch --pool_size 0
