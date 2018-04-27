2c75527b529862240e3f47dea7b58298f2bdac0e
Ken Yu <cn.ken.yu@gmail.com>
2018-04-27 03:20:44 -0400

    Fix bugs of N2R training.

--- scripts/train_n2r.sh
+++ scripts/train_n2r.sh
@@ -1,2 +1,3 @@
-python train.py --dataroot ./datasets/rigid --name n2r_pix2pix --model pix2pix --which_model_netG unet_256\
-    --which_direction BtoA --lambda_A 100 --dataset_mode randomly_masked --no_lsgan --norm batch --pool_size 0
+python train.py --dataroot ../datasets/rigid --name n2r_pix2pix --model pix2pix --which_model_netG unet_256\
+    --which_direction BtoA --lambda_A 100 --dataset_mode randomly_masked --no_lsgan --norm batch --pool_size 0\
+    --gpu_ids 0,1,3
